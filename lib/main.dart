7fcdcaf00d099cd8ab7e0c272e6485899a592712
Emmy <eemmyy.a@gmail.com>
2023-08-31 16:05:15 +0700

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ask me anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  void changeBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(4) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+          child: TextButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () => {changeBallNumber()},
+      )),
+    );
+  }
+}
